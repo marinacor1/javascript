329e35abf9be5f162ef84d60b5dd1c44d9fa8628
marinacor1 <marinaspam1234@gmail.com>
2016-05-06 13:51:25 -0600

    preliminary if else strcture written:

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,5 +1,12 @@
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 for (i = 0; i< array.length, i++) {
-  console.log(array[i])
+  if array[i]%3 === 0 && array[i] %5 === 0
+   console.log("FizzBuzz");
+  else if array[i]% 3 === 0
+    console.log("Fizz");
+  else if array[i]%5 === 0
+    console.log("Buzz");
+  else
+   console.log(array[i])
 }
