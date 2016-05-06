361eee311902bb9ce8fea89fe5a4ec7266dfbc2f
marinacor1 <marinaspam1234@gmail.com>
2016-05-06 16:12:14 -0600

    Added correct code for family

---
+++ family_constructor_array.js
@@ -0,0 +1,17 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+};
+
+var family = [];
+
+family[0] = new Person("alice", 40);
+family[1] = new Person("bob", 42);
+family[2] = new Person("michelle", 8);
+family[3] = new Person("timmy", 6);
+
+// loop through our new array
+
+for (i = 0; i < family.length; i ++) {
+  console.log(family[i].name);
+};
--- more_this_practice.js
+++ more_this_practice.js
@@ -13,3 +13,19 @@ function Rectangle(height, width) {
 var rex = new Rectangle(7,3);
 var area = rex.calcArea();
 var perimeter = rex.calcPerimeter();
+
+function Rabbit(adjective) {
+    this.adjective = adjective;
+    this.describeMyself = function() {
+        console.log("I am a " + this.adjective + " rabbit");
+    };
+}
+
+// now we can easily make all of our rabbits
+var rabbit1 = new Rabbit ("fluffy");
+var rabbit2 = new Rabbit ("happy");
+var rabbit3 = new Rabbit ("sleepy");
+
+rabbit1.describeMyself
+rabbit2.describeMyself
+rabbit3.describeMyself
