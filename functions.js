7ea4e08b383844e8f47bdafb9af82362d093ca95
marinacor1 <marinaspam1234@gmail.com>
2016-05-06 14:12:33 -0600

    Adding basic functions practice

---
+++ functions.js
@@ -0,0 +1,12 @@
+// Accepts a number x as input and returns its square
+var square = function (x) {
+  return x * x;
+};
+
+// Write the function multiply below
+// It should take two parameters and return the product
+var multiply = function (x, y) {
+  return x * y;
+};
+
+multiply(3, 4);
