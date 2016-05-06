b0795c54c3bdcc76b5f115fd71504bf752b5631d
marinacor1 <marinaspam1234@gmail.com>
2016-05-06 14:24:28 -0600

    Adding rectanlge practice

--- this.js
+++ this.js
@@ -28,3 +28,21 @@ susan.age = 25;
 // here, update Susan's age to 35 using the method
 susan.setAge = setAge;
 susan.setAge(35);
+
+var rectangle = new Object();
+rectangle.height = 3;
+rectangle.width = 4;
+// here is our method to set the height
+rectangle.setHeight = function (newHeight) {
+  this.height = newHeight;
+};
+// help by finishing this method
+rectangle.setWidth = function (newWidth) {
+  this.width = newWidth;
+};
+
+// here change the width to 8 and height to 6 using our new methods
+rectangle.height = rectangle.setHeight;
+rectangle.setHeight(6);
+rectangle.width = rectangle.setWidth;
+rectangle.setWidth(8);
