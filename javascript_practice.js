a573f51c616ef115a1d0f16a783d562c95c3a85e
marinacor1 <marinaspam1234@gmail.com>
2016-05-06 13:31:33 -0600

    First commit

---
+++ javascript_practice.js
@@ -0,0 +1,14 @@
+var friends = {
+    bill: {
+        firstName: "Bill",
+        lastName: "Gates",
+        number: "2312321233",
+        address: ["One Microsoft Lane", "Redmond", "WA", "98954"]
+        }
+    };
+    
+var list = function(friends){
+  for(var key in object) {
+   console.log("Hi");
+  }
+};
