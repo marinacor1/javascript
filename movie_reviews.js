40f170404b7f7e7e3fb7e7e782cdf94cb1fbb350
marinacor1 <marinaspam1234@gmail.com>
2016-05-06 13:58:15 -0600

    preliminary use of switch

---
+++ movie_reviews.js
@@ -0,0 +1,15 @@
+var getReview = function (movie) {
+  switch(movie) {
+   case "Toy Story 2":
+      return "Great story. Mean prospectorl";
+      break;
+    case "Finding Nemo":
+      return "Cool animation, and funny turles.";
+      break;
+    case "The Lion King":
+      return "Great songs.";
+      break;
+    default:
+      return "I don't know!";
+  }
+};
