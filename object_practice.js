a525bb15cadbf76934c8fa54f8148bd106fc2887
marinacor1 <marinaspam1234@gmail.com>
2016-05-06 14:04:38 -0600

    Two ways to access information

--- object_practice.js
+++ object_practice.js
@@ -14,3 +14,15 @@ var name2 = susan.name;
 var age2 = susan.age;
 
 //Known as properties and values, instead of keys and values
+
+// Take a look at our next example object, a dog
+var dog = {
+  species: "greyhound",
+  weight: 60,
+  age: 4
+};
+
+var species = dog["species"];
+// fill in the code to save the weight and age using bracket notation
+var weight = dog["weight"];
+var age = dog["age"];
