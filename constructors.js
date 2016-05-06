25c0fb960160ff359fa5795b44d51557226d5e98
marinacor1 <marinaspam1234@gmail.com>
2016-05-06 15:54:03 -0600

    Added defaults within constructs

--- constructors.js
+++ constructors.js
@@ -8,3 +8,27 @@ var bob = new Person("Bob Smith", 30);
 var susan = new Person("Susan Jordan", 25);
 // help us make george, whose name is "George Washington" and age is 275
 var george = new Person("George Washington", 275);
+
+function Cat(age, color) {
+  this.age = age;
+  this.color = color;
+}
+
+// make a Dog constructor here
+function Dog(age, color, breed) {
+ this.age = age;
+ this.color = color;
+ this.breed = breed;
+}
+
+//set defaults
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.species = "Homo Sapiens";
+}
+
+var sally = new Person ("Sally Bowles", 39);
+var holden = new Person ("Holden Caulfield", 16);
+console.log("sally's species is " + sally.species + " and she is " + sally.age);
+console.log("holden's species is " + holden.species + " and he is " + holden.age);
