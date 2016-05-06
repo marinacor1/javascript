d8a8bd196940f3605622f27164ca6bd4756071f2
marinacor1 <marinaspam1234@gmail.com>
2016-05-06 13:41:25 -0600

    Adding steve and search function

--- javascript_practice.js
+++ javascript_practice.js
@@ -4,11 +4,26 @@ var friends = {
         lastName: "Gates",
         number: "2312321233",
         address: ["One Microsoft Lane", "Redmond", "WA", "98954"]
+        },
+        steve: {
+         firstName: "Steve",
+         lastName: "Jobs",
+         number: "920384032",
+         address: ["lsdjfs", "lskjf", "lsdkj", "sa", "sldfj"]
         }
     };
-    
+
 var list = function(friends){
   for(var key in object) {
    console.log("Hi");
   }
+};
+
+var search = function (name){
+    for (var key in friends) {
+        if (friends[key].firstName === name) {
+  console.log(friends[key]);
+  return friends[key];
+    }
+}
 };
