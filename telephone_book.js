777f1d6f7f83d3d7cbde9f7f9830a43ee72fb7b9
marinacor1 <marinaspam1234@gmail.com>
2016-05-07 08:14:59 -0600

    preliminary code

---
+++ telephone_book.js
@@ -0,0 +1,28 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list {
+  var contactsLength = contacts.length
+  for (i = 0; i < contactsLength - 1; i ++) {
+       printPerson(contacts[i]);
+  }
+};
+
+list;
