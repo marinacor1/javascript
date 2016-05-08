78f67626bc204ff04d6ca457ee8dc8027391811c
marinacor1 <marinaspam1234@gmail.com>
2016-05-08 10:28:31 -0600

    Added more functionalit to prototype

--- prototype.js
+++ prototype.js
@@ -33,3 +33,20 @@ Animal.prototype.sayName = function () {
 // provided code to test above constructor and method
 var penguin = new Animal("Captain Cook", 2);
 penguin.sayName();
+
+
+function Dog (breed) {
+    this.breed = breed;
+};
+
+// add the sayHello method to the Dog class
+// so all dogs now can say hello
+Dog.prototype.sayHello = function() {
+  console.log("Hello this is a " + this.breed + " dog");
+};
+
+var yourDog = new Dog("golden retriever");
+yourDog.sayHello();
+
+var myDog = new Dog("dachshund");
+myDog.sayHello();
