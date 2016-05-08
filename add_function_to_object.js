0a426232c9652a6491e99be0f5a17d9423cd9aa3
marinacor1 <marinaspam1234@gmail.com>
2016-05-08 08:59:20 -0600

    Changed inside method to say job which prints out a property within the object using this.

--- add_function_to_object.js
+++ add_function_to_object.js
@@ -23,3 +23,23 @@ var james = {
 
 james.speak("great");
 james.speak("just okay");
+
+//with properties that are within the object
+
+var james = {
+    job: "programmer",
+    married: false,
+    sayJob: function() {
+      console.log("Hi, I work as a " + this.job);
+      // complete this metho
+    }
+};
+
+// james' first job
+james.sayJob();
+
+// change james' job to "super programmer" here
+james.job = "super programmer";
+
+// james' second job
+james.sayJob();
