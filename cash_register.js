fac3dc5e1fe2c0c6553829fc815583f3707a56ea
marinacor1 <marinaspam1234@gmail.com>
2016-05-08 10:50:51 -0600

    Refactored to have quantity

--- cash_register.js
+++ cash_register.js
@@ -1,35 +1,22 @@
 var cashRegister = {
-    total: 0,
+    total:0,
     add: function(itemCost){
-      this.total += itemCost;
+        this.total += itemCost;
     },
-    scan: function (item) {
+    scan: function(item, quantity) {
         switch (item) {
-        case "eggs":
-            this.add(0.98);
-            break;
-
-        case "milk":
-            this.add(1.23);
-            break;
-        case "magazine":
-            this.add(4.99);
-            break;
-        case "chocolate":
-            this.add(0.45);
-            break;
-
+        case "eggs": this.add(0.98 * quantity); break;
+        case "milk": this.add(1.23 * quantity); break;
+        case "magazine": this.add(4.99 * quantity); break;
+        case "chocolate": this.add(0.45 * quantity); break;
         }
-        return true;
     }
 };
 
-cashRegister.scan("eggs");
-cashRegister.scan("eggs");
-cashRegister.scan("magazine");
-cashRegister.scan("magazine");
-cashRegister.scan("magazine");
-//Scan 2 eggs and 3 magazines
+cashRegister.scan("eggs", 4);
+cashRegister.scan("milk", 4);
+cashRegister.scan("magazine", 4);
+cashRegister.scan("chocolate", 4);
 
 //Show the total bill
 console.log('Your bill is '+cashRegister.total);
