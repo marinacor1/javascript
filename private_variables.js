e36b34398fde8c83727c1ab22754bdac042d5c33
marinacor1 <marinaspam1234@gmail.com>
2016-05-08 10:01:29 -0600

    added private variables

---
+++ private_variables.js
@@ -0,0 +1,13 @@
+function Person(first,last,age) {
+   this.firstname = first;
+   this.lastname = last;
+   this.age = age;
+   var bankBalance = 7500;
+}
+
+// create your Person
+var john = new Person("John", "Loquito", 43);
+
+// try to print his bankBalance
+console.log(john.bankBalance);
+//prints as undefined
