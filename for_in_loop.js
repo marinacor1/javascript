a823082cbebbed8bbded631f8c7eaeac52fc1c6d
marinacor1 <marinaspam1234@gmail.com>
2016-05-08 09:16:00 -0600

    Set a var

--- for_in_loop.js
+++ for_in_loop.js
@@ -8,3 +8,17 @@ var nyc = {
 for (var property in nyc) {
  console.log(property);
 }
+
+//set a variable for property
+
+var nyc = {
+    fullName: "New York City",
+    mayor: "Bill de Blasio",
+    population: 8000000,
+    boroughs: 5
+};
+
+// write a for-in loop to print the value of nyc's properties
+for (var x in nyc) {
+ console.log(nyc[x]);
+}
