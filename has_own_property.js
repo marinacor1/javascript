8fc5bffb4133f382044ac7b751701b9f9849643e
marinacor1 <marinaspam1234@gmail.com>
2016-05-08 09:12:20 -0600

    Added conditional

--- has_own_property.js
+++ has_own_property.js
@@ -5,3 +5,15 @@ var myObj = {
 
 console.log( myObj.hasOwnProperty('name') ); // should print true
 console.log( myObj.hasOwnProperty('nickname') ); // should print false
+
+
+var suitcase = {
+    shirt: "Hawaiian"
+};
+
+if (suitcase.hasOwnProperty('shorts')) {
+  console.log(suitcase.shorts);
+} else {
+    suitcase.shorts = "khaki";
+    console.log(suitcase.shorts);
+  }
