e44ce4311ab283f4391efed6c46793f53b14faa5
marinacor1 <marinaspam1234@gmail.com>
2016-05-08 08:52:18 -0600

    added function to object

---
+++ add_function_to_object.js
@@ -0,0 +1,13 @@
+function Person(job, married) {
+    this.job = job;
+    this.married = married;
+    // add a "speak" method to Person!
+    this.speak = function speak() {
+        console.log("Hello!");
+    }
+
+    
+}
+
+var user = new Person("Codecademy Student",false);
+user.speak();
