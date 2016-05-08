8891d5b4380a413b5f579749d168449a4026f6ab
marinacor1 <marinaspam1234@gmail.com>
2016-05-08 09:35:42 -0600

    more practice added

--- prototype.js
+++ prototype.js
@@ -13,3 +13,23 @@ buddy.bark();
 var snoopy = new Dog("Beagle");
 /// this time it works!
 snoopy.bark();
+
+// create your Animal class here
+function Animal (name, numLegs) {
+ this.name= name,
+ this.numLegs= numLegs
+};
+
+Animal.prototype.sayName = function () {
+ console.log("Hi my name is " + this.name);
+};
+
+// create the sayName method for Animal
+
+
+
+
+
+// provided code to test above constructor and method
+var penguin = new Animal("Captain Cook", 2);
+penguin.sayName();
